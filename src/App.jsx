c0da021755037aab7779f58428c51cf0331e653f
elvisespinozaN <elvis.espinoza.navarrete@outlook.com>
2025-03-11 20:56:07 -0400

    react router configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>React Router</h1>
+    <div id="container">
+      <div id="navbar">
+        <Routes>
+          <Route path="/blue" element={<h1>Blue</h1>} />
+        </Routes>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="red" element={<h1>Red</h1>} />
+        </Routes>
+      </div>
     </div>
   );
 }
